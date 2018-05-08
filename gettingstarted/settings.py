7ca984242ccc3e7f007ed127e739c79acf23ab32
biplovdahal <root@calvisitor-10-105-150-102.calvisitor.1918.berkeley.edu>
2018-05-07 20:59:39 -0700

    first push

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'hello',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
