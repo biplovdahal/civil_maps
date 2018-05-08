92d2b36fabe9a9a9b1b42f786795f0d6f4747b10
biplovdahal <root@Biplovs-MacBook-Pro.local>
2018-05-08 15:24:01 -0700

    fixes

--- hello/views.py
+++ hello/views.py
@@ -22,14 +22,16 @@ class placesAPI(APIView):
             if results[items].get('photos'):
                 photo_reference = results[items]['photos'][0]['photo_reference']
                 #key = 'AIzaSyDVmmJxaTel3f6q-dk5BY7VTpy_RSVLpug'
+                dictionary['a_href'] = results[items]['photos'][0]['html_attributions'][0].replace('<a href=\"','')
                 key='AIzaSyAXXy92G1yHeMbNAkva737XCVGcXbIE4aE'
                 dictionary['height'] = results[items]['photos'][0]['height']
+                dictionary['place_id'] = results[items]['place_id']
                 dictionary['image_url'] = ('https://maps.googleapis.com/maps/api/place/photo?maxwidth=250&photoreference='+photo_reference+'&sensor=false&key='+key)
-
+                dictionary['iframe_url'] = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAmmWZcWiCzouqxF-GMgI4RjJmnOjD2AJM&q="+results[items]['formatted_address'].replace(' ','+')
             else:
                 dictionary['image_url'] = None
             dictionary['address'] = results[items]['formatted_address']
 
             output_arr.append(dictionary)
-        print("This is data", output_arr)
+        #print("This is data", output_arr)
         return Response(output_arr)
