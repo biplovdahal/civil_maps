36fa4ff4345d602c1bb69a06f8654b42b1b50b19
biplovdahal <root@Biplovs-MacBook-Pro.local>
2018-05-08 16:49:36 -0700

    initial push

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,30 +1,6 @@
 import { FETCH_POSTS, NEW_POST, FETCH_PLACES } from './types';
 
 
-// export const fetchPosts = (location) => dispatch => {
-//   console.log('this is latitude', latitude)
-//   if(latitude){
-//     fetch('http://127.0.0.1:8000?latitude='+latitude+'&longitude='+longitude)
-//       .then(res => res.json())
-//       .then(posts =>
-//         dispatch({
-//           type: FETCH_POSTS,
-//           payload: posts
-//         })
-//       );
-//
-//   }else{
-//     fetch('https://jsonplaceholder.typicode.com/posts')
-//       .then(res => res.json())
-//       .then(posts =>
-//         dispatch({
-//           type: FETCH_POSTS,
-//           payload: posts
-//         })
-//       );
-//   };
-//   }
-
 export const savePlace = myPlace => dispatch =>{
 
   dispatch({
@@ -35,8 +11,8 @@ export const savePlace = myPlace => dispatch =>{
 
 
 export const createPost = location => dispatch => {
-  fetch('https://vast-beach-76093.herokuapp.com?location='+location.location+'',{
-  //fetch('http://127.0.0.1:8000?location='+location.location+'', {
+  //fetch('https://vast-beach-76093.herokuapp.com?location='+location.location+'',{
+  fetch('http://127.0.0.1:8000?location='+location.location+'', {
     method: 'GET',
 
     headers: {
